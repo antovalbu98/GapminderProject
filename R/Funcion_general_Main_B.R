63b653d6cbd96bbe53d53dce0ca7b6b4100899e3
Borjaa-P <71317591+Borjaa-P@users.noreply.github.com>
2020-12-19 17:30:46 +0100

    funcion general

--- R/Funcion_general_Main_B.R
+++ R/Funcion_general_Main_B.R
@@ -4,6 +4,7 @@ imputadorMissing <- function() {
   
   tryCatch(expr = {
   
+    # Función para leer config y valiudar(script config)
     
     loginfo("Leyendo el config...", logger = 'log')
     config <- leerConfig()
@@ -11,6 +12,7 @@ imputadorMissing <- function() {
     loginfo("Config leido.", logger = 'log')
     
     
+    #Funciones leer datos y crear df(scrip leer datos)
     
     loginfo("Empezamos la app...", logger = 'log')
   
@@ -19,23 +21,51 @@ imputadorMissing <- function() {
     loginfo("Se han leido los datos", logger = 'log')
     
    
-     
+     loginfo("Empezamos la transformaccion de los datos", logger = 'log')
     
-    
-    
-    loginfo("Empezamos la transformaccion de los datos", logger = 'log')
-    
-     dftotal<-creacionDataFrame(datas, config)
+     dftotal <-creacionDataFrame(datas, config)
     
     loginfo("Se ha creado df completo ", logger = 'log')
     
     
+    #Funcion para limpieza de datos (script limpieza de datos)
+    
     loginfo("Empezamos la limpieza  de los datos", logger = 'log')
     
-    dffinal<-limpiar_datos(dftotal)
+    dffinal <-limpiar_datos(dftotal)
     
     loginfo("Los datos ya estan limpios ", logger = 'log')
     
+    # funciones para Generar modelo y predecir.(Script generar modelo)
+    
+    
+    loginfo("Empezamos separando los datos en train", logger = 'log')
+    
+    df_train <-separa_train(dffinal,config)
+    
+    loginfo("Los datos ya estan separados ", logger = 'log')
+    
+    
+    
+    loginfo("Empezamos separando los datos de test", logger = 'log')
+    
+    df_test <-separa_test(dffinal,config)
+    
+    loginfo("Los datos ya estan separados en test ", logger = 'log')
+    
+    
+    loginfo("Empezamos a generar el modelo ", logger = 'log')
+    
+    modelo <-entrenar_modelo(dffinal)
+    
+    loginfo("El modelo se ha generado  ", logger = 'log')
+    
+    
+    loginfo("Empezamos a predecir", logger = 'log')
+    
+    predicción <-predecir_objetivo(modelo)
+    
+    loginfo("Los prediccion ya esta hecha", logger = 'log')
     
     
     
