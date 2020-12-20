af48d2babdd61fe3cdc7adf7e1536343da0859a8
antovalbu98 <antovalbu98@gmail.com>
2020-12-20 13:20:23 +0100

    validateConfigNodes anadido

--- R/Config_BP.R
+++ R/Config_BP.R
@@ -20,7 +20,34 @@ leerConfig <- function(){
              logger = 'log')
     stop()
   })
+  validateConfigNodes(config) # le anado la nueva funcion,
+                              # creo que el resto de cosas de la de ander
+                              # no aplican
+  
   
 }
   
+
+####---- Validar config nodes ----
+
+validateConfigNodes <- function(config){
+  
+  nodoPrincipal <- identical(names(config), c("input", "columnas"))
+  nodoInput <- identical(names(config$input), c("name"))
+  nodoColumnas <- identical(names(config$columnas), c("pais_objetivo", "year_objetivo"))
+  nodos <- c("nodoPrincipal" = nodoPrincipal, "nodoInput" = nodoInput, 
+             "nodoColumnas" = nodoColumnas)
+  check <- all(nodos)
+  if(!check){
+    
+    nodosMalos <- names(nodos)[!nodos]
+    
+    logerror(paste0("Los nodos: ", paste(nodosMalos, collapse = ", "),
+                    " estan mal estructurados!"), logger = 'log')
+    stop()
+    
+  }
+}
+
+
  
